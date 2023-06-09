115dd232db23e10e7873b24921c155c0a7d81dae
Nikita Dymont <gg.joke55@gmail.com>
2023-06-09 11:05:35 +0300

    implement login page

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,3 +1,33 @@
+import { useState } from "react";
+
+import styles from "./style.module.css";
+
 export const Login = () => {
-  return <div>login</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <>
+      <h1 className={styles.login_title}>Login Page</h1>
+      <form className={styles.form}>
+        <input
+          className={styles.form_input}
+          type="text"
+          placeholder="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          className={styles.form_input}
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className={styles.form_button} type="submit">
+          Submit
+        </button>
+      </form>
+    </>
+  );
 };
